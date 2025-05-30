574987ed6bcb624e52f36283d01dfb3c69a5a13b
Urban <urban.mutagen@gmail.com>
2025-05-30 20:29:01 +0300

    dz ready)

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,5 @@
-export const PATH_DB =
+import * as path from 'node:path';
+
+// export const PATH_DB = 'src/db/db.json';
+// export const PATH_DB = path.join('src', 'db', 'db.json');
+export const PATH_DB = path.resolve('src', 'db', 'db.json');
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,18 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    const contacts = Array(number).fill(0).map(createFakeContact);
+
+    const prevContacts = await readContacts();
+
+    await writeContacts([...prevContacts, ...contacts]);
+  } catch (err) {
+    console.error('Помилка в generateContacts:', err);
+    throw err;
+  }
+};
 
 generateContacts(5);
