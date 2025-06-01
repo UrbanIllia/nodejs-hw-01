15740c2ce5d816311aa8f529ac058e8451706754
Urban <urban.mutagen@gmail.com>
2025-06-01 19:42:46 +0300

    the same test

--- src/utils/createFakeContact.js
+++ src/utils/createFakeContact.js
@@ -2,10 +2,17 @@ import { faker } from '@faker-js/faker';
 
 export const createFakeContact = () => ({
   id: faker.string.uuid(),
-  aircraftType: faker.airline.aircraftType(),
-  airplane: faker.airline.airplane(),
-  airport: faker.airline.airport(),
-  flightNumber: faker.airline.flightNumber(),
-  seat: faker.airline.seat(),
+  name: faker.person.fullName(),
+  phone: faker.phone.number(),
+  email: faker.internet.email(),
+  job: faker.person.jobTitle(),
 });
-// console.log(createFakeContact());
+// export const createFakeContact = () => ({
+//   id: faker.string.uuid(),
+//   aircraftType: faker.airline.aircraftType(),
+//   airplane: faker.airline.airplane(),
+//   airport: faker.airline.airport(),
+//   flightNumber: faker.airline.flightNumber(),
+//   seat: faker.airline.seat(),
+// });
+console.log(createFakeContact());
